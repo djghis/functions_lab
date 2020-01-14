b3f29768078a08983148ab15555eafdeb07d6d04
djghis <djghis@hotmail.com>
2020-01-14 16:42:16 +0000

    Initial commit

---
+++ month.rb
@@ -0,0 +1,3 @@
+1 = "january"
+2 = "february"
+3 = "march"
---
+++ ruby_functions_practice.rb
@@ -0,0 +1,46 @@
+def return_10()
+  return 10
+end
+
+def add(num1, num2)
+  return  num1 + num2
+end
+
+def subtract(num1, num2)
+  return num1 - num2
+end
+
+def multiply(num1, num2)
+  return num1 * num2
+end
+
+def divide(num1, num2)
+  return num1 / num2
+end
+
+def length_of_string(string)
+ return string.length
+end
+
+
+def join_string(string1, string2)
+  return string1 + string2
+
+end
+
+def add_string_as_number(string1, string2)
+  return string1.to_i + string2.to_i
+end
+
+
+def number_to_full_month_name(num)
+  if num == 1
+    return "January"
+  end
+end
+
+def number_to_full_month_name(num)
+  if num == 3
+    return "March"
+  end
+end
